95944e4e9a34ce7c14961c421ccc31ca88e1c0f1
Rokas <rookas.rudzenskas@gmail.com>
2022-12-03 15:48:54 +0200

    smart

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/q-ytv6uCThJNRv2uh_zVxTB0m1rbsSKj',
+      accounts: [
+        '<YOUR_RINKEBY_ACCOUNT_PRIVATE_KEY_GOES_HERE>',
+      ],
+    },
+  },
+}
