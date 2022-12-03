877e622710144755c0490357b8e1553888d7d61f
Rokas <rookas.rudzenskas@gmail.com>
2022-12-03 15:54:55 +0200

    Add private key to hardhat.config.js
    This is a temporary solution to allow us to deploy the contracts to Goerli.
    
    We should remove this commit before we merge this branch into master.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.0',
   networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/q-ytv6uCThJNRv2uh_zVxTB0m1rbsSKj',
-      accounts: [
-        '<YOUR_RINKEBY_ACCOUNT_PRIVATE_KEY_GOES_HERE>',
-      ],
+      url: "https://eth-goerli.g.alchemy.com/v2/q-ytv6uCThJNRv2uh_zVxTB0m1rbsSKj",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
     },
   },
 }
